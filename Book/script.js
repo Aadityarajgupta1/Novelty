d692e5f426218ad6d94bfcf4e75f7a49d8a7556f
Samir Thapaliya <samirthapaliya2@gmail.com>
2023-08-01 21:36:56 +0545

    managed

--- Book/script.js
+++ Book/script.js
@@ -1,111 +1,97 @@
 $(document).ready(function () {
-
-    $('.increment-btn').click(function (e) {
-         e.preventDefault();
-
-         var qty = $(this).closest('.product_data').find('.input-qty').val();
-         
-         var value = parseInt(qty, 10);
-         value = isNaN(value) ? 0 : value;
-         if(value < 10)
-         {
-            value++;
-            $(this).closest('.product_data').find('.input-qty').val(value);
-         }
-    });
-
-    $('.decrement-btn').click(function (e) {
-        e.preventDefault();
-
-        var qty = $(this).closest('.product_data').find('.input-qty').val();
-        
-        var value = parseInt(qty, 10);
-        value = isNaN(value) ? 0 : value;
-        if(value > 1)
-        {
-           value--;
-           $(this).closest('.product_data').find('.input-qty').val(value);
-        }
-   });
-
-   $('.addToCartBtn').click(function (e){
+  $(".increment-btn").click(function (e) {
     e.preventDefault();
-   //  var qty = $(this).closest('.product_data').find('.input-qty').val();
-   var qty = '1';
-    var prod_id = $(this).val();
+    var qty = $(this).closest(".product_data").find(".input-qty").val();
+    var value = parseInt(qty, 10);
+    value = isNaN(value) ? 0 : value;
+    if (value < 10) {
+      value++;
+      $(this).closest(".product_data").find(".input-qty").val(value);
+    }
+  });
 
-     $.ajax({
-         method: "POST",
-         url: "../Login/functions/handlecart.php",
-         data: {
-            "prod_id": prod_id,
-            "prod_qty": qty,
-            "scope": "add"
-         },
-         success: function (response){
-            if(response == 201)
-            {
+  $(".decrement-btn").click(function (e) {
+    e.preventDefault();
+
+    var qty = $(this).closest(".product_data").find(".input-qty").val();
+
+    var value = parseInt(qty, 10);
+    value = isNaN(value) ? 0 : value;
+    if (value > 1) {
+      value--;
+      $(this).closest(".product_data").find(".input-qty").val(value);
+    }
+  });
+
+  $(".addToCartBtn").click(function (e) {
+   e.preventDefault();
+   var qty = $(this).closest(".product_data").find(".input-qty").val();
+   var prod_id = $(this).val();
+
+   if (qty < 1 || qty == null) {
+       qty = 1;
+   }
+
+   $.ajax({
+       method: "POST",
+       url: "../Login/functions/handlecart.php",
+       data: {
+           prod_id: prod_id,
+           prod_qty: qty,
+           scope: "add",
+       },
+       success: function (response) {
+           if (response == 201) {
                alert("Product added to cart");
-            }
-            else if(response == "Existing")
-            {
-               alert("Product alredy on cart");
-            }
-            else if(response == 401)
-            {
+           } else if (response == "Existing") {
+               alert("Product already on cart");
+           } else if (response == 401) {
                alert("Login to continue");
-            }
-            else if(response == 500)
-            {
+           } else if (response == 500) {
                alert("Something went wrong");
-            }
-         }
-
-     });
+           }
+       },
    });
+});
 
-   $(document).on('click','.updateQty', function () {
-      var qty = $(this).closest('.product_data').find('.input-qty').val();
 
-      var prod_id = $(this).closest('.product_data').find('.prodId').val();
+  $(document).on("click", ".updateQty", function () {
+    var qty = $(this).closest(".product_data").find(".input-qty").val();
 
-      $.ajax({
-         method: "POST",
-         url: "../Login/functions/handlecart.php",
-         data: {
-            "prod_id": prod_id,
-            "prod_qty": qty,
-            "scope": "update"
-         },
-         success: function (response)
-         {
-               // alert(response);
-         }
-      })
-   });
+    var prod_id = $(this).closest(".product_data").find(".prodId").val();
 
-   $(document).on('click','.deleteItem', function () {
-        var cart_id = $(this).val();
-      //   alert(cart_id);
-      $.ajax({
-         method: "POST",
-         url: "../Login/functions/handlecart.php",
-         data: {
-            "cart_id": cart_id,
-            "scope": "delete"
-         },
-         success: function (response)
-         {
-            if(response == 200)
-            {
-               alert("Product deleted from cart");
-               $('#myReload').load(location.href + " #myReload");
-            }
-            else
-            {
-               alert(response);
-            }
-         }
-      });
-   });
+    $.ajax({
+      method: "POST",
+      url: "../Login/functions/handlecart.php",
+      data: {
+        prod_id: prod_id,
+        prod_qty: qty,
+        scope: "update",
+      },
+      success: function (response) {
+        // alert(response);
+      },
+    });
+  });
+
+  $(document).on("click", ".deleteItem", function () {
+    var cart_id = $(this).val();
+    //   alert(cart_id);
+    $.ajax({
+      method: "POST",
+      url: "../Login/functions/handlecart.php",
+      data: {
+        cart_id: cart_id,
+        scope: "delete",
+      },
+      success: function (response) {
+        if (response == 200) {
+          alert("Product deleted from cart");
+          $("#myReload").load(location.href + " #myReload");
+        } else {
+          alert(response);
+        }
+      },
+    });
+  });
 });
