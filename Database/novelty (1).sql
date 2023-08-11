c46c929122249f9d789a5c171f71bdda2f0c263a
Samir Thapaliya <samirthapaliya2@gmail.com>
2023-08-11 15:32:31 +0545

    managed

--- Database/novelty (1).sql
+++ Database/novelty (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 09, 2023 at 04:59 PM
+-- Generation Time: Aug 11, 2023 at 11:46 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -133,6 +133,13 @@ CREATE TABLE `orders` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `tracking_no`, `user_id`, `name`, `email`, `phone`, `address`, `city`, `total_price`, `payment_mode`, `payment_id`, `status`, `comments`, `created_at`) VALUES
+(65, 'aadi2428', 36, 'Daquan Rocha', 'wymohax@mailinator.com', '17', 'Duis proident sint ', 'A sed qui alias enim', 638, 'COD', '-', 0, NULL, '2023-08-10 09:06:04');
+
 -- --------------------------------------------------------
 
 --
@@ -147,6 +154,13 @@ CREATE TABLE `order_items` (
   `price` int(191) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `order_items`
+--
+
+INSERT INTO `order_items` (`id`, `order_id`, `prod_id`, `qty`, `price`, `created_at`) VALUES
+(246, 65, 47, 1, 638, '2023-08-10 09:06:04');
 
 -- --------------------------------------------------------
 
@@ -185,7 +199,7 @@ CREATE TABLE `products` (
 INSERT INTO `products` (`id`, `category_id`, `name`, `slug`, `small_description`, `description`, `original_price`, `selling_price`, `image`, `qty`, `author`, `page_count`, `weight`, `isbn`, `language`, `status`, `trending`, `meta_title`, `meta_keywords`, `meta_description`, `created_at`) VALUES
 (45, 54, 'Rich Dad Poor Dad', 'aadi6898book', 'April 2017 marks 20 years since Robert Kiyosaki\'s Rich Dad Poor Dad first made waves in the Personal Finance arena. It has since become the #1 Personal Finance book of all time... translated into dozens of languages and sold around the world. Rich Dad Poor Dad is Robert\'s story of growing up with two dads -- his real father and the father of his best friend, his rich dad -- and the ways in which both men shaped his thoughts about money and investing. The book explodes the myth that you need to earn a high income to be rich and explains the difference between working for money and having your money work for you', 'April 2017 marks 20 years since Robert Kiyosaki\'s Rich Dad Poor Dad first made waves in the Personal Finance arena. It has since become the #1 Personal Finance book of all time... translated into dozens of languages and sold around the world. Rich Dad Poor Dad is Robert\'s story of growing up with two dads -- his real father and the father of his best friend, his rich dad -- and the ways in which both men shaped his thoughts about money and investing. The book explodes the myth that you need to earn a high income to be rich and explains the difference between working for money and having your money work for you', 300, 500, '1691430058.jpg', 295, 'Robert T. Kiyoaki', '336 Pages', '245g', '9781612680194', 'English', 0, 0, '', 'Paper Back', 'Business,\r\nBusiness and Investing,\r\nFinance,\r\nEconomics,\r\nInvesting,\r\nSelf Improvement and Relationships', '2023-08-07 17:40:58'),
 (46, 53, 'September Love', 'aadi3999book', 'A book that will change the way you think about love, relationships, heartbreak, and self-empowerment. Breaking the rules, challenging perceptions, and exploring the secret desires we keep hidden from the world. Beautifully composed and written by international bestselling author Lang Leav, this new collection of poetry and prose will positively influence your life. September Love captures the magic of each passing season, a pearl of wisdom waiting to be discovered with every page turned. A book that will inspire you to reach for the stars.', 'A book that will change the way you think about love, relationships, heartbreak, and self-empowerment. Breaking the rules, challenging perceptions, and exploring the secret desires we keep hidden from the world. Beautifully composed and written by international bestselling author Lang Leav, this new collection of poetry and prose will positively influence your life. September Love captures the magic of each passing season, a pearl of wisdom waiting to be discovered with every page turned. A book that will inspire you to reach for the stars.', 400, 1200, '1691430969.jpg', 298, 'Lang Leav', '224 Pages', '240g', '9781524859596', 'English', 0, 0, 'September Love', 'Paper Back', 'Fiction and Literature,\r\nPoetry and Prose', '2023-08-07 17:56:09'),
-(47, 54, 'Deep Work', 'aadi3719book', 'Cal Newport discusses in his new book, Deep Work: Rules For Focused Success In A Distracted World, about how professionals of today have started valuing quantity over quality; and how this has turned young professionals of today into puppets who try to indulge in extensive multitasking, dealing with multiple emails and projects. This prevents them from doing \'deep work\'; which is focused work free from all other distractions. This also means that the professionals of today should sort out their priorities. Newport uses principles of psychology and neuroscience to enhance his points.', 'Cal Newport discusses in his new book, Deep Work: Rules For Focused Success In A Distracted World, about how professionals of today have started valuing quantity over quality; and how this has turned young professionals of today into puppets who try to indulge in extensive multitasking, dealing with multiple emails and projects. This prevents them from doing \'deep work\'; which is focused work free from all other distractions. This also means that the professionals of today should sort out their priorities. Newport uses principles of psychology and neuroscience to enhance his points.', 300, 638, '1691431062.jpg', 297, 'Cal Newport', '304 Pages', '230g', '9780349413686', 'English', 0, 0, 'Deep Work', 'Paper Back', 'Business,\r\nBusiness and Investing,\r\nSelf Improvement and Relationships,\r\nSelf Help,\r\nProductivity,\r\nTime Management,\r\nHistory, Biography, and Social Science', '2023-08-07 17:57:42'),
+(47, 54, 'Deep Work', 'aadi3719book', 'Cal Newport discusses in his new book, Deep Work: Rules For Focused Success In A Distracted World, about how professionals of today have started valuing quantity over quality; and how this has turned young professionals of today into puppets who try to indulge in extensive multitasking, dealing with multiple emails and projects. This prevents them from doing \'deep work\'; which is focused work free from all other distractions. This also means that the professionals of today should sort out their priorities. Newport uses principles of psychology and neuroscience to enhance his points.', 'Cal Newport discusses in his new book, Deep Work: Rules For Focused Success In A Distracted World, about how professionals of today have started valuing quantity over quality; and how this has turned young professionals of today into puppets who try to indulge in extensive multitasking, dealing with multiple emails and projects. This prevents them from doing \'deep work\'; which is focused work free from all other distractions. This also means that the professionals of today should sort out their priorities. Newport uses principles of psychology and neuroscience to enhance his points.', 300, 638, '1691431062.jpg', 296, 'Cal Newport', '304 Pages', '230g', '9780349413686', 'English', 0, 0, 'Deep Work', 'Paper Back', 'Business,\r\nBusiness and Investing,\r\nSelf Improvement and Relationships,\r\nSelf Help,\r\nProductivity,\r\nTime Management,\r\nHistory, Biography, and Social Science', '2023-08-07 17:57:42'),
 (48, 53, 'One of Us Is Lying', 'aadi9952book', 'On Thursday afternoon, five students at Bayview High walk into detention. Bronwyn,the brain, is Yale-bound and never breaks a rule. Addy, the beauty, is the picture-perfect homecoming princess. Nate,the criminal,is already on probation for dealing. Cooper,the athlete,is the all-star baseball pitcher. And Simon,the outcast,is the creator of Bayview High\'s notorious gossip app. Only, Simon never makes it out of that classroom.', 'On Thursday afternoon, five students at Bayview High walk into detention. Bronwyn,the brain, is Yale-bound and never breaks a rule. Addy, the beauty, is the picture-perfect homecoming princess. Nate,the criminal,is already on probation for dealing. Cooper,the athlete,is the all-star baseball pitcher. And Simon,the outcast,is the creator of Bayview High\'s notorious gossip app. Only, Simon never makes it out of that classroom.', 300, 638, '1691431139.jpg', 300, 'Karen M. Mcmanus', '368 Pages', '270g', '9780141375632', 'English', 0, 0, '', 'Paper Back', 'Fiction and Literature,\r\nMystery, Thriller & Suspense,\r\nYoung Adult', '2023-08-07 17:58:59'),
 (50, 54, 'Atomic Habits', 'aadi7014book', 'Transform your life with tiny changes in behaviour - starting now. *The instant New York Times bestseller* *Financial Times Book of the Month* People think when you want to change your life, you need to think big. But world-renowned habits expert James Clear has discovered another way. He knows that real change comes from the compound effect of hundreds of small decisions - doing two push-ups a day, waking up five minutes early, or holding a single short phone call. ', 'Transform your life with tiny changes in behaviour - starting now. *The instant New York Times bestseller* *Financial Times Book of the Month* People think when you want to change your life, you need to think big. But world-renowned habits expert James Clear has discovered another way. He knows that real change comes from the compound effect of hundreds of small decisions - doing two push-ups a day, waking up five minutes early, or holding a single short phone call. ', 500, 1278, '1691431585.png', 299, 'James Clear', '320 Pages', '350g', '9781847941831', 'English', 0, 0, '', 'Jacketed', 'Business,\r\nBusiness and Investing,\r\nSelf Improvement and Relationships,\r\nSelf Help,\r\nLeadership,\r\nProductivity,\r\nTime Management,\r\nHistory, Biography, and Social Science', '2023-08-07 18:06:25'),
 (51, 53, 'Red, White & Royal Blue', 'aadi7660book', '* Instant NEW YORK TIMES and USA TODAY bestseller ** GOODREADS CHOICE AWARD WINNER for BEST DEBUT and BEST ROMANCE of 2019 ** BEST BOOK OF THE YEAR* for VOGUE, NPR, VANITY FAIR, and more! *What happens when America\'s First Son falls in love with the Prince of Wales?When his mother became President, Alex Claremont-Diaz was promptly cast as the American equivalent of a young royal. Handsome, charismatic, genius—his image is pure millennial-marketing gold for the White House. There\'s only one problem: Alex has a beef with the actual prince, Henry, across the pond. And when the tabloids get hold of a photo involving an Alex-Henry altercation, U.S./British relations take a turn for the worse. Heads of family, state, and other handlers devise a plan for damage control: staging a truce between the two rivals. ', '* Instant NEW YORK TIMES and USA TODAY bestseller ** GOODREADS CHOICE AWARD WINNER for BEST DEBUT and BEST ROMANCE of 2019 ** BEST BOOK OF THE YEAR* for VOGUE, NPR, VANITY FAIR, and more! *What happens when America\'s First Son falls in love with the Prince of Wales?When his mother became President, Alex Claremont-Diaz was promptly cast as the American equivalent of a young royal. Handsome, charismatic, genius—his image is pure millennial-marketing gold for the White House. There\'s only one problem: Alex has a beef with the actual prince, Henry, across the pond. ', 400, 958, '1691431930.jpg', 297, 'Casey Mcquiston', '432 Pages', '320g', '9781250316776', 'English', 0, 0, '', 'Paper Back', 'Fiction and Literature,\r\nLGBTQIA,\r\nRomance,\r\nYoung Adult', '2023-08-07 18:12:10'),
@@ -309,7 +323,7 @@ ALTER TABLE `blogs`
 -- AUTO_INCREMENT for table `carts`
 --
 ALTER TABLE `carts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=305;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=306;
 
 --
 -- AUTO_INCREMENT for table `categories`
@@ -327,19 +341,19 @@ ALTER TABLE `news`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
 
 --
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=246;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=247;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -356,7 +370,7 @@ ALTER TABLE `users`
 --
 ALTER TABLE `carts`
   ADD CONSTRAINT `for` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `forn` FOREIGN KEY (`prod_id`) REFERENCES `products` (`id`);
+  ADD CONSTRAINT `forn` FOREIGN KEY (`prod_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `orders`
@@ -369,13 +383,13 @@ ALTER TABLE `orders`
 --
 ALTER TABLE `order_items`
   ADD CONSTRAINT `foreign` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
-  ADD CONSTRAINT `frn` FOREIGN KEY (`prod_id`) REFERENCES `products` (`id`);
+  ADD CONSTRAINT `frn` FOREIGN KEY (`prod_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `products`
 --
 ALTER TABLE `products`
-  ADD CONSTRAINT `fori` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
+  ADD CONSTRAINT `fori` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
